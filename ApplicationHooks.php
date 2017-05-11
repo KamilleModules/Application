1ac2dd80d97510743f86890f8124995829cd6107
lingtalfi <lingtalfi@gmail.com>
2017-05-11 15:04:29 +0200

    update

--- ApplicationHooks.php
+++ ApplicationHooks.php
@@ -9,7 +9,8 @@ class ApplicationHooks
 
     protected static function Core_feedEarlyRouter(\Module\Core\Architecture\Router\EarlyRouter $router)
     {
-        $router->addRouter(\Module\Application\Architecture\Router\MaintenanceRouter::create());
+
+
     }
 }
 

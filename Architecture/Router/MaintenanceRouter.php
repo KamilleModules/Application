0fafa92594563d70f01c1c51499ea7e2173e6aaa
lingtalfi <lingtalfi@gmail.com>
2017-05-14 10:00:04 +0200

    update

--- Architecture/Router/MaintenanceRouter.php
+++ Architecture/Router/MaintenanceRouter.php
@@ -5,11 +5,10 @@ namespace Module\Application\Architecture\Router;
 
 
 use Kamille\Architecture\Request\Web\HttpRequestInterface;
-use Kamille\Architecture\Router\Helper\RouterHelper;
-use Kamille\Architecture\Router\RouterInterface;
+use Kamille\Architecture\Router\Web\WebRouterInterface;
 use Kamille\Services\XConfig;
 
-class MaintenanceRouter implements RouterInterface
+class MaintenanceRouter implements WebRouterInterface
 {
 
     public static function create()
@@ -20,7 +19,7 @@ class MaintenanceRouter implements RouterInterface
     public function match(HttpRequestInterface $request)
     {
         if (true === XConfig::get("Application.maintenanceMode")) {
-            return RouterHelper::routerControllerToCallable(XConfig::get("Application.maintenanceController"));
+            return XConfig::get("Application.maintenanceController");
         }
     }
 }
